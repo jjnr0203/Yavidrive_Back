222dc5b0a5b3ef0d20fdbd747b9ff80686dc1e7c
migue1angel <macf06200@gmail.com>
2023-12-01 15:54:39 -0500

    pruebas

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,14 +1,17 @@
-import express from 'express';
+import express, { response } from 'express';
 import connectionDB from '../database';
 const router = express.Router();
+router.use(express.json());
 
 router.get('/', (req, res) => {
-    connectionDB.query("SELECT * FROM users where cedula = $1 && password = $2",
+    connectionDB.query("SELECT * FROM users where email = $1 or cedula = $1 and password = $2",
     [req.body.user,req.body.password], (error, results) => {
-        if (error) {
-            throw error;
+        if (results.rows.length > 0) {
+            res.send(JSON.stringify('Usuario encontrado'));
+        }else{
+            res.send (JSON.stringify('Usuario no encontrado'))
         }
-        res.send(JSON.stringify('Usuario encontrado'))
+
     });
 });
 
