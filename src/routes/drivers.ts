b1be123888966231df8f10d15272f7769ac9337b
migue1angel <macf06200@gmail.com>
2023-12-10 17:22:05 -0500

    creacion de drivers y vehicle

---
+++ src/routes/drivers.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import connectionDB from '../database';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  connectionDB.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.send(results.rows);
+  });
+});
+
+router.get("/:id", (req: any, res: any) => {
+  connectionDB.query("select * from driver where id_user = $1", [req.params.id], (error: any, results: any) => {
+    if (error) {
+      throw error;
+    }
+    res.send(results.rows);
+  });
+});
+
+router.put('/:id', (req: any, res: any) => {
+  connectionDB.query("update users set cedula=$1, password=$2, email=$3, id_role=$4 where id_user = $5",
+    [req.body.cedula, req.body.password, req.body.email, req.body.id_role, req.params.id], (error: any, results: any) => {
+      if (error) {
+        throw error;
+      }
+      res.json('Actualizado exitosamente');
+    });
+});
+
+router.post('/', async (req: any, res: any) => {
+  try {
+    console.log(req.body)
+    const newDriver = req.body;
+    const driver = await connectionDB.query(
+      'INSERT INTO drivers (name_d, lastname_d, photo_d, phone_d,user_id_d) VALUES ($1, $2, $3, $4, $5)RETURNING id_driver',
+      [req.body.name, req.body.lastname, req.body.photo, req.body.phone, req.body.user_id]);
+    const newDriverID = {
+      id_driver: driver.rows[0].id_driver,
+      ...newDriver
+    }
+    res.send(newDriverID);
+  } catch (error) {
+    res.send('Error al crear el conductor');
+    console.log(error);
+  }
+});
+
+
+router.delete("/:id", (req: any, res: any) => {
+  connectionDB.query("delete from users where id_user = $1", [req.params.id], (error: any, results: any) => {
+    if (error) {
+      throw error;
+    }
+    res.json('eliminado correctamente');
+  });
+});
+
+export default router;
+
+
---
+++ src/routes/vehicle.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import connectionDB from '../database';
+const router = express.Router();
+
+router.post('/',(req,res)=>{
+    try {
+        connectionDB.query('Insert into vehicle(registration, color, model, brand, num_seats,driver_id) values ($1,$2,$3,$4,$5,$6) ', [req.body.registration,req.body.color,req.body.model,req.body.brand,req.body.numSeats,req.body.idDriver,])
+        res.json('vehiculo creado exitosamente')
+    } catch (error) {
+        console.log(error);
+        res.send('Error al crear el vehiculo')
+    }
+})
+
+export default router;
