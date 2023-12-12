888a1eeb8edfe6f7ee826bfc56a8b35df18bb909
migue1angel <macf06200@gmail.com>
2023-12-11 22:06:05 -0500

    router new

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import usersRouter from './routes/users';
 import loginRouter from './routes/login';
 import routesRouter from './routes/routes';
 import registerRouter from './routes/register';
+import vehicleRouter from './routes/vehicle';
+import driversRouter from './routes/drivers';
 const cors = require('cors')
 
 app.use(cors());
@@ -13,6 +15,8 @@ app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/routes", routesRouter);
 app.use("/register", registerRouter);
+app.use("/vehicle", vehicleRouter);
+app.use("/drivers", driversRouter);
 
 app.get('/roles', (req:any, res:any) => {
     connectionDB.query("SELECT * FROM roles", (error, results) => {
--- src/routes/vehicle.ts
+++ src/routes/vehicle.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/',(req,res)=>{
     try {
         connectionDB.query('Insert into vehicle(registration, color, model, brand, num_seats,driver_id) values ($1,$2,$3,$4,$5,$6) ', [req.body.registration,req.body.color,req.body.model,req.body.brand,req.body.numSeats,req.body.idDriver,])
-        res.json('vehiculo creado exitosamente')
+        res.send('vehiculo creado exitosamente')
     } catch (error) {
         console.log(error);
         res.send('Error al crear el vehiculo')
