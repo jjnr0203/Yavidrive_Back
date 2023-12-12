5151542d65faba00a94d035e6f86f57244097f95
migue1angel <macf06200@gmail.com>
2023-12-12 11:00:05 -0500

    correccion creacion de vehiculo

--- src/routes/vehicle.ts
+++ src/routes/vehicle.ts
@@ -5,10 +5,10 @@ const router = express.Router();
 router.post('/',(req,res)=>{
     try {
         connectionDB.query('Insert into vehicle(registration, color, model, brand, num_seats,driver_id) values ($1,$2,$3,$4,$5,$6) ', [req.body.registration,req.body.color,req.body.model,req.body.brand,req.body.numSeats,req.body.idDriver,])
-        res.send('vehiculo creado exitosamente')
+        res.json('vehiculo creado exitosamente')
     } catch (error) {
         console.log(error);
-        res.send('Error al crear el vehiculo')
+        res.json('Error al crear el vehiculo')
     }
 })
 
