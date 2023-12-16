55a1476a26595f3b4680d3479a99327ec22c32a5
j0n4t8an <morajonathanjm24@gmail.com>
2023-12-16 06:20:32 -0500

    register complete

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import connectionDB from '../database';
 const router = express.Router();
 
-router.get('/service', (req, res) => {
-    connectionDB.query("SELECT * FROM customer,register, routes, drivers, vehicle, zone WHERE register.customer_id=1  AND customer.id_customer=1 and register.routes_id=routes.id_routes AND routes.driver_id=drivers.id_driver AND vehicle.driver_id=drivers.id_driver AND routes.zone_id=zone.id_zone", (error, results) => {
+router.get('/:id', (req : any, res : any) => {
+    connectionDB.query("SELECT register.start_date, register.end_date, routes.description, routes.price, zone.name AS name_z, drivers.name AS name_d, drivers.lastname AS lastname_d, drivers.photo AS photo_d, drivers.phone AS phone_d, vehicle.registration, vehicle.color, vehicle.model, vehicle.brand FROM register,  routes,  zone, drivers, vehicle, customer WHERE register.id_customer=$1  AND customer.id_customer=$1 and register.id_routes=routes.id_routes AND routes.id_driver=drivers.id_driver AND vehicle.id_driver=drivers.id_driver AND routes.id_zone=zone.id_zone", [req.params.id], (error, results) => {
         if (error) {
             throw error;
         }
@@ -12,8 +12,8 @@ router.get('/service', (req, res) => {
     });
 });
 
-router.get('/clients', (req, res) => {
-    connectionDB.query("SELECT * FROM drivers,customer, register, routes, zone WHERE register.customer_id=1  AND customer.id_customer=1 AND register.routes_id=routes.id_routes AND routes.driver_id=drivers.id_driver", (error, results) => {
+router.get('/d/:id', (req : any, res : any) => {
+    connectionDB.query("SELECT register.start_date, register.end_date, customer.name AS name_c, customer.lastname AS lastname_c, customer.photo AS photo_c, customer.phone AS phone_c FROM register, customer, routes, drivers WHERE register.id_customer=$1  AND customer.id_customer=$1 AND register.id_routes=routes.id_routes AND routes.id_driver=drivers.id_driver", [req.params.id], (error, results) => {
         if (error) {
             throw error;
         }
