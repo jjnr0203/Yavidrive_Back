53bba15bf73b98ea4ec44d92eaef125c2f65918b
DILANIUP <dilanurquizo29112004@gmail.com>
2023-12-20 00:17:24 -0500

    cambios home

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,8 @@ import connectionDB from '../database';
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
-    connectionDB.query("SELECT customer.name, customer.photo, customer.lastname, roles.rol_name FROM customer, users, roles WHERE customer.user_id = users.id_user AND users.id_user = '1' AND roles.id_role = users.id_role",
+    connectionDB.query("SELECT customer.name as name_c, customer.photo, customer.lastname, roles.name FROM customer, users, roles WHERE customer.user_id = users.id_user AND users.id_user = $1 AND roles.id_role = users.role_id",
+    [req.params.id],
         (error, results) => {
             if (error) {
                 throw error;
@@ -12,14 +13,16 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
-    connectionDB.query("SELECT drivers.name, drivers.photo, drivers.lastname, roles.rol_name FROM drivers, users, roles WHERE drivers.user_id = users.id_user AND users.id_user = '11' AND roles.id_role = users.id_role",
+router.get('/drivers/:id', (req, res) => {
+    connectionDB.query("SELECT drivers.name as name_d, drivers.photo, drivers.lastname, roles.name FROM drivers, users, roles WHERE drivers.user_id = users.id_user AND users.id_user = $1 AND roles.id_role = users.role_id",
+    [req.params.id],
         (error, results) => {
             if (error) {
                 throw error;
             }
             res.send(results.rows[0]);
         });
+    
 });
 
 
